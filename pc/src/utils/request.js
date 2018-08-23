73af773396356b510596aa71ea291e31da4433bd
calamus0427 <1211994507@qq.com>
2018-08-23 11:14:33 +0800

    地址对接

--- pc/src/utils/request.js
+++ pc/src/utils/request.js
@@ -1,8 +1,17 @@
 /*
     路径配置
 */
+
 var url = {
-    'dev': {},
+    'dev': {
+        "weixinLogin":"http://yapi.demo.qunar.com/mock/16780/wexin_login",
+        "foodList":"http://yapi.demo.qunar.com/mock/16780/get_food",
+        "orderFood":"http://yapi.demo.qunar.com/mock/16780/order_food",
+        "getUser":"http://yapi.demo.qunar.com/mock/16780/get_user",
+        "getUserTag":"http://yapi.demo.qunar.com/mock/16780/get_user_tag",
+        "getInfo":"http://yapi.demo.qunar.com/mock/16780/get_info",
+        "getFoodTag":"http://yapi.demo.qunar.com/mock/16780/get_food_tag",
+    },
     'prod': {}
 
 }
