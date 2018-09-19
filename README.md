497c21c0bb1958c319607fafef446aee83c1cefb
calamus0427 <1211994507@qq.com>
2018-09-19 16:13:28 +0800

    renew readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# wechat-jc
-jingchen
+# 微信订餐小程序
+
+[文档说明](https://calamus0427.github.io/wechat-order/#/)
+[管理后台预览](https://calamus0427.github.io/wechat-order/pc/dist/index.html#/login)
+
+## 代码说明
+- /wechat 小程序代码
+- /pc 管理后代代码
+- /docs 文档说明源码（文档说明中的截图展示未更新，具体参考项目示例
+
+## 扫码体验
--- wechat/dist/_wepylogs.js
+++ wechat/dist/_wepylogs.js
@@ -1,1 +1,3 @@
 console.log('WePY开启错误监控');
+console.error("CLI报错：Missing files: ../components/imageList in /Users/calamus/calamus/my-project/wechat-jc/wechat/src/pages/order.wpy");
+console.error("CLI报错：找不到组件：/Users/calamus/calamus/my-project/wechat-jc/wechat/src/components/imageList\n请尝试使用 npm install /Users/calamus/calamus/my-project/wechat-jc/wechat/src/components/imageList 安装");
